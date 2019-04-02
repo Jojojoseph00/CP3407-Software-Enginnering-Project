2c28d70aa2685418ed0a0c18e1c7e5f29f3dbb32
JosephQQQ <elmoukhtar.mokhtari@my.jcu.edu.au>
2019-04-02 16:38:17 +0800

    Setting up branch

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  text = "Northern Lights is green to go...";
 
+  onChangeText() {
+    this.text = "Machine is Active, Welcome Mr Finche";
+    console.log("Log to test");
+  }
 }
