4cc475f133774079f556d42a01990f2b3596b8d4
JosephQQQ <elmoukhtar.mokhtari@my.jcu.edu.au>
2019-05-29 01:44:31 +0800

    Restyle

--- src/app/newfeed/newfeed.page.ts
+++ src/app/newfeed/newfeed.page.ts
@@ -1,30 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import {ActivatedRoute} from '@angular/router';
-import * as firebase from 'firebase';
-import { RouterTestingModule } from '@angular/router/testing';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
+import * as firebase from "firebase";
+import { RouterTestingModule } from "@angular/router/testing";
 
 @Component({
-  selector: 'app-newfeed',
-  templateUrl: './newfeed.page.html',
-  styleUrls: ['./newfeed.page.scss'],
+  selector: "app-newfeed",
+  templateUrl: "./newfeed.page.html",
+  styleUrls: ["./newfeed.page.scss"]
 })
 export class NewfeedPage implements OnInit {
-
-  uid:string;
-  constructor(public activeRoute:ActivatedRoute,public router: Router,) { 
-    this.uid=this.activeRoute.snapshot.paramMap.get('uid');
-
+  uid: string;
+  constructor(public activeRoute: ActivatedRoute, public router: Router) {
+    this.uid = this.activeRoute.snapshot.paramMap.get("uid");
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  chatUser(){
-    this.router.navigate(["chats", firebase.auth().currentUser.uid])
+  chatUser() {
+    this.router.navigate(["chats", firebase.auth().currentUser.uid]);
   }
-  userProfile(){
-    this.router.navigate(["profile",firebase.auth().currentUser.uid])
+  userProfile() {
+    this.router.navigate(["profile", firebase.auth().currentUser.uid]);
   }
-  
 }
