ac20e783bc65e554cbf7a84f83d0c5dba347cd4d
JosephQQQ <elmoukhtar.mokhtari@my.jcu.edu.au>
2019-05-30 02:20:57 +0800

    Proper item page test

--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -17,14 +17,14 @@ export class ShopService {
       id: "i2",
       title: "Iphone X",
       imageUrl:
-        "https://cdn.thewirecutter.com/wp-content/uploads/2018/06/mirrorless-camera-2x1-fullres-2024-1024x512.jpg",
+        "https://store.stormfront.co.uk/content/images/thumbs/0006975_iphone-x.jpeg",
       description: ["new condition", "low price", "some bs"]
     },
     {
       id: "i3",
       title: "Macbook Pro",
       imageUrl:
-        "https://cdn.thewirecutter.com/wp-content/uploads/2018/06/mirrorless-camera-2x1-fullres-2024-1024x512.jpg",
+        "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/image/AppleInc/aos/published/images/m/bp/mbp13/space/mbp13-space-select-201807_GEO_SG?wid=892&hei=820&&qlt=80&.v=1531167604448",
       description: ["new condition", "low price", "some bs"]
     }
   ];
