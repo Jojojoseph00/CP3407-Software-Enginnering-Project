702dd734c340e67fb5a3f3d70983da8509f4c8ab
JosephQQQ <elmoukhtar.mokhtari@my.jcu.edu.au>
2019-05-29 13:04:18 +0800

    fix bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,26 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'newfeed/:uid', loadChildren: './newfeed/newfeed.module#NewfeedPageModule' },
-  { path: 'chats/:uid', loadChildren: './chats/chats.module#ChatsPageModule' },
-  { path: 'searchusers/:uid', loadChildren: './searchusers/searchusers.module#SearchusersPageModule' },
-  { path: 'contactchat/:uid', loadChildren: './contactchat/contactchat.module#ContactchatPageModule' },
-  { path: 'profile/:uid', loadChildren: './profile/profile.module#ProfilePageModule' },
-
-
-
-
-
-
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", loadChildren: "./home/home.module#HomePageModule" },
+  {
+    path: "newfeed/:uid",
+    loadChildren: "./newfeed/newfeed.module#NewfeedPageModule"
+  },
+  { path: "chats/:uid", loadChildren: "./chats/chats.module#ChatsPageModule" },
+  {
+    path: "searchusers/:uid",
+    loadChildren: "./searchusers/searchusers.module#SearchusersPageModule"
+  },
+  {
+    path: "contactchat/:uid",
+    loadChildren: "./contactchat/contactchat.module#ContactchatPageModule"
+  },
+  {
+    path: "profile/:uid",
+    loadChildren: "./profile/profile.module#ProfilePageModule"
+  }
 ];
 
 @NgModule({
@@ -23,4 +29,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
