d585f1115aea69aed97ab7c116401edd5a37158b
JosephQQQ <elmoukhtar.mokhtari@my.jcu.edu.au>
2019-05-30 09:41:24 +0800

    fix

--- src/app/userlisting/userlisting.page.ts
+++ src/app/userlisting/userlisting.page.ts
@@ -1,38 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
-import { ItemsService } from '../items.service';
-import * as firebase from 'firebase';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AlertController } from "@ionic/angular";
+import { ItemsService } from "../items.service";
+import * as firebase from "firebase";
 
 @Component({
-  selector: 'app-userlisting',
-  templateUrl: './userlisting.page.html',
-  styleUrls: ['./userlisting.page.scss'],
+  selector: "app-userlisting",
+  templateUrl: "./userlisting.page.html",
+  styleUrls: ["./userlisting.page.scss"]
 })
 export class UserlistingPage implements OnInit {
   items = [];
   uid: string;
-  constructor(public router: Router,public alertCtrl: AlertController,public itemservice:ItemsService) {
+  constructor(
+    public router: Router,
+    public alertCtrl: AlertController,
+    public itemservice: ItemsService
+  ) {
     this.itemservice.getlistingitems().then((res: any) => {
-      console.log(res)
+      console.log(res);
       this.items = res;
     });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   userProfile() {
     this.router.navigate(["profile", firebase.auth().currentUser.uid]);
   }
 
-  newFeed(){
-    this.router.navigate(["newfeed", firebase.auth().currentUser.uid])
+  newFeed() {
+    this.router.navigate(["newfeed", firebase.auth().currentUser.uid]);
   }
-  userRented(){
+  userRented() {
     this.router.navigate(["userrented", firebase.auth().currentUser.uid]);
-
   }
-  chatUser(){
-    this.router.navigate(["chats", firebase.auth().currentUser.uid])
+  chatUser() {
+    this.router.navigate(["chats", firebase.auth().currentUser.uid]);
+  }
+
+  userListing() {
+    this.router.navigate(["userlisting", firebase.auth().currentUser.uid]);
   }
 }
